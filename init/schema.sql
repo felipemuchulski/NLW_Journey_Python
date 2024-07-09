9e89212a015cf84579a96a2fb8547ea213b360ec
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-07-08 21:25:40 -0300

    Adicionada pasta init

---
+++ init/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS 'trips' (
+    id_trips TEXT PRIMARY KEY,
+    destination TEXT NOT NULL, 
+    start_date DATETIME,
+    end_date DATETIME,
+    owner_name TEXT NOT NULL,
+    owner_email TEXT NOT NULL,
+    status INTEGER -- 1 para verdadeiro (true) , 0 para falso (false)
+);
+
+CREATE TABLE IF NOT EXISTS 'emails_to_invite' (
+    id_invite TEXT PRIMARY KEY,
+    trip_id TEXT,
+    email TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id_trips)
+);
+
+CREATE TABLE IF NOT EXISTS 'links' (
+    id_links TEXT PRIMARY KEY,
+    trip_id TEXT,
+    link TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id_trips)
+);
