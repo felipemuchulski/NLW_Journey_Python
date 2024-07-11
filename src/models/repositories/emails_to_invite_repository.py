6a912151fe07e113e9471369bb3f1393eda7eb7f
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2024-07-10 23:11:29 -0300

    Criada inserção de dados

---
+++ src/models/repositories/emails_to_invite_repository.py
@@ -0,0 +1,33 @@
+from typing import Dict, Tuple, List
+from sqlite3 import Connection
+
+class EmailsToInviteRepository:
+    def __init__(self, conn: Connection) -> None:
+        self.__conn = conn
+    
+    def registry_email(self, emails_infos: Dict) -> None:
+        cursor = self.__conn.cursor();
+        cursor.execute(
+            '''
+                INSERT INTO emails_to_invite (
+                 id_invite, trip_id, email)
+                VALUES(?, ?, ?)
+
+            ''', (
+                emails_infos["id_invite"],
+                emails_infos["trip_id"],
+                emails_infos["email"]
+            )
+        )
+        self.__conn.commit();
+
+    ## Buscar exemplos no banco de dados
+    def find_email_from_trip(self, trip_id: str) -> list[Tuple]:
+        cursor = self.__conn.cursor();
+        cursor.execute(
+        ''' SELECT * FROM emails_to_invite WHERE trip_id = ?''', (trip_id,)
+        )
+        trip = cursor.fetchall()
+        return trip
+    
+   
